273b31ec18c2fe8d217e7054871928b004e8917c
juratali <juratbekxudayberganov9@gmail.com>
2023-11-23 16:49:25 +0500

    route checking

--- src/middlewares/PathChecker.tsx
+++ src/middlewares/PathChecker.tsx
@@ -1,24 +1,24 @@
 import { Navigate, useLocation } from "react-router-dom";
 
 export const PathChecker = ({ children }: { children: any }) => {
-  const accessToken = localStorage.getItem("accessToken");
-  const authStep = localStorage.getItem("authStep");
+  // const accessToken = localStorage.getItem("accessToken");
+  // const authStep = localStorage.getItem("authStep");
 
-  const location = useLocation();
+  // const location = useLocation();
 
-  if (location.pathname === "/auth/confirm") {
-    if (!authStep || authStep !== "VERIFY_EMAIL") {
-      return <Navigate to="/auth/login" replace={true} />;
-    }
-  }
+  // if (location.pathname === "/auth/confirm") {
+  //   if (!authStep || authStep !== "VERIFY_EMAIL") {
+  //     return <Navigate to="/auth/login" replace={true} />;
+  //   }
+  // }
 
-  if (!accessToken) {
-    <Navigate to="/auth/login" replace={true} />;
-  }
+  // if (!accessToken) {
+  //   <Navigate to="/auth/login" replace={true} />;
+  // }
 
-  if (accessToken && location.pathname.includes("auth")) {
-    return <Navigate to="/" replace={true} />;
-  }
+  // if (accessToken && location.pathname.includes("auth")) {
+  //   return <Navigate to="/" replace={true} />;
+  // }
 
   return <>{children}</>;
 };
